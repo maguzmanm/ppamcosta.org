90d8997c08c42aad49f693864324f2cc0ef7dcdc
maguzmanm <maguzmanm@gmail.com>
2026-06-10 10:41:59 -0400

    fix: cache-bust PWA icons and manifest

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -28,22 +28,22 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/icon-32.png',
+            src: '/icon-32.png?v=2',
             sizes: '32x32',
             type: 'image/png',
           },
           {
-            src: '/icon-192.png',
+            src: '/icon-192.png?v=2',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icon-512.png',
+            src: '/icon-512.png?v=2',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/icon-512.png',
+            src: '/icon-512.png?v=2',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
